0be209d061eef6a56d9ef9a727fd24b419aef808
Emanuil Gospodinov <gospodinove@gmail.com>
2022-04-05 15:11:43 +0300

    Custom localization keys

--- cartridges/app_custom2/cartridge/controllers/Product.js
+++ cartridges/app_custom2/cartridge/controllers/Product.js
@@ -1,15 +1,20 @@
 'use strict';
 
 var customPreferences = require('dw').system.Site.current.preferences.custom;
+var Resource = require('dw/web/Resource');
+
 var server = require('server');
 var page = module.superModule;
 
 server.extend(page);
 
 server.append('Show', function (req, res, next) {
+    var customerName = customer.profile ? customer.profile.firstName : "guest"
+
     var viewData = res.getViewData();
 
     viewData.customSitePreference = customPreferences.customSitePreference;
+    viewData.name = Resource.msgf('training2.template.name', 'training2', null, customerName)
 
     res.setViewData(viewData);
     next();
