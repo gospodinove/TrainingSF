e9a2257d1fb3d234ab67f170ea03355be8f2217f
Emanuil Gospodinov <gospodinove@gmail.com>
2022-04-07 15:06:31 +0300

    Display form validation error message

--- cartridges/app_custom2/cartridge/client/default/js/form.js
+++ cartridges/app_custom2/cartridge/client/default/js/form.js
@@ -1,18 +1,21 @@
 "use strict";
 
+var validation = require('../../../../../app_storefront_base/cartridge/client/default/js/components/formValidation.js')
+
 $(document).ready(function() {
   $("form.training2").on("submit", function(e) {
     e.preventDefault();
 
-    var url = $(this).attr('action');
-    var values = $(this).serialize();
+    var form = $(this);
+    var url = form.attr('action');
+    var values = form.serialize();
 
     $.ajax({
       url: url,
       type: 'post',
       data: values,
       success: function (data) {
-        console.log(data)
+        validation(form, data);
       },
       error: function(err) {
         console.log(err)
